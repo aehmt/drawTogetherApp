c23b2481fe92f5a266503a64288246c70c329ddd
satub <satu.barnhill@gmail.com>
2016-11-17 15:16:08 -0500

    dummy routes and files added for artworks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'welcome#home'
+
+  namespace :api do
+    namespace :v1 do
+      resources :artworks
+    end
+  end
   
 end
